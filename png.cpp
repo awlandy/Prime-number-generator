bbe2d62f9ddff614bd1e1266cb5814e44f15d940
Andy <40180962+awlandy@users.noreply.github.com>
2019-04-26 15:54:57 +0800

    Update png.cpp

--- png.cpp
+++ png.cpp
@@ -4,7 +4,11 @@
 #include <time.h>
 #include <random>
 
-const char zerotonine[] = "123456789";
+constexpr int PRIME_NUMBER_DIGITS = 16;	// this cannot go bigger than 18 with long long or 19 with unsigned long long
+
+const char zerotonine[] = "0123456789";
+const char onetonine[] = "123456789";
+const char odd[] = "13579";
 
 bool prime(long long n) {
 	long limit = sqrt(n);
@@ -19,8 +23,11 @@ bool prime(long long n) {
 }
 
 long long getlldrandom() {
-	char buf[17] = "";
-	for (int index = 0; index < 16; ++index) {
+	char buf[PRIME_NUMBER_DIGITS + 1] = "";
+
+	buf[0] = onetonine[(std::rand() % (strlen(onetonine)))];
+	buf[PRIME_NUMBER_DIGITS - 1] = odd[(std::rand() % (strlen(odd)))];
+	for (int index = 1; index < (PRIME_NUMBER_DIGITS - 1); ++index) {
 		buf[index] = zerotonine[(std::rand() % (strlen(zerotonine)))];
 	}
 	return atoll(buf);
@@ -30,12 +37,14 @@ int main() {
 	srand(time(NULL));
 	long long randomprime;
 	bool stop = false;
-	while(1) {
+	while (1) {
 		randomprime = getlldrandom();
 		bool stop = prime(randomprime);
-		printf("This is %s prime number %lld\n", stop ? "true" : "false", randomprime);		//just to show me whats goin' on
-		if (stop)
-			break;
+#ifdef _DEBUG	// #ifdef DEBUG for other compilers
+		printf("This is %s prime number %lld\n", stop ? "true" : "false", randomprime);
+#endif
+			if (stop)
+				break;
 	}
 	printf("Success: Random 16-digit prime %lld\n", randomprime);		//print result
 	system("pause");
